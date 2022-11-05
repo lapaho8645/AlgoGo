3f0adfe63f88d1fb402d51728b95db06010ecf2d
lapaho8645 <64197428+lapaho8645@users.noreply.github.com>
2022-11-05 21:06:07 +0900

    [level 2] Title: 숫자의 표현, Time: 0.03 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,8 +1,11 @@
 import java.util.*;
 class Solution {
     public int solution(int n) {
-        
-        int sub = 3;
+        // 15-3 = 12 
+        // 12 % 2 == 0? 0이면 answer 증가시키기
+        // 15-6 == 9
+        // 9 % 3 == 0? 0이면 answer증가시키기
+        int sub = 3;            
         int plusNum = 3;
         int seq = 2;
         int answer = 1;

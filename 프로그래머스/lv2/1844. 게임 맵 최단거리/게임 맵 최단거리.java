2df5edc6754bfeffec5639ffbbf98f1e33ffcfdc
lapaho8645 <64197428+lapaho8645@users.noreply.github.com>
2022-11-10 18:38:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.40 ms, Memory: 53.4 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
-    static int [] dx = {0, 1, 0, -1};
-    static int [] dy = {1, 0, -1, 0};
+    static int [] dx = {1, -1, 0, 0};
+    static int [] dy = {0, 0, 1, -1};
     public int solution(int[][] maps) {
         int answer = 0;
         
@@ -9,31 +9,31 @@ class Solution {
         queue.add(new locate(0,0));
         int m = maps[0].length;
         int n = maps.length;
-        int [][] visited = new int[n][m];
-        visited[0][0] = 1;
+        boolean [][] visited = new boolean[n][m];
+        System.out.println("n :" + n + " m : " + m);
         boolean isPossible = false;
         while(!queue.isEmpty()){
             locate cur = queue.poll();
-        
+            visited[cur.x][cur.y] = true;
+            System.out.println(cur.x + " : " + cur.y);
+            answer++;
             if(cur.x == n-1 && cur.y == m-1){
                 isPossible = true;
                 break;
             }
             
             for(int i = 0; i < 4; i++){
+                
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
-                
-                if(nx < 0 || nx >= n || ny < 0 || ny >= m || visited[nx][ny] > 0 || maps[nx][ny] == 0)
-                    continue;
-
-                queue.add(new locate(cur.x + dx[i],cur.y + dy[i]));
-                visited[nx][ny] = visited[cur.x][cur.y] + 1;
-               
+  
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && maps[nx][ny] == 1){
+                    queue.add(new locate(cur.x + dx[i],cur.y + dy[i]));
+                }
             }
             
        }
-        return isPossible ? visited[n-1][m-1] : -1;
+        return isPossible ? answer : -1;
     }
     class locate{
         int x;

d4915979fce987e8723ce3551ef12091586f6dd8
lapaho8645 <64197428+lapaho8645@users.noreply.github.com>
2022-11-30 15:15:47 +0900

    [Silver III] Title: N과 M (3), Time: 480 ms, Memory: 114936 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int m, n;
+	static int [] input, numbers;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		n = sc.nextInt();
+		m = sc.nextInt();
+		input = new int[n];
+		numbers = new int[m];
+	
+		for(int i = 0; i < n; i++) {
+			input[i] = i+1;
+		}
+		permu(0);
+		System.out.println(sb.toString());
+		
+	}
+	public static void permu(int cnt) {
+		if(cnt == m) {
+			for(int i = 0; i < m; i++) {
+				sb.append(numbers[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = 0; i < n; i++) {
+			numbers[cnt] = input[i];
+			permu(cnt+1);
+		}
+	}
+
+}

ef37569639b988d0136b8b88520bce3565a14558
lapaho8645 <64197428+lapaho8645@users.noreply.github.com>
2022-11-30 13:42:04 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int m, n;
+	static int [] input, numbers;
+	static boolean [] isSelected;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		n = sc.nextInt();
+		m = sc.nextInt();
+		input = new int[n];
+		numbers = new int[m];
+		isSelected = new boolean[n];
+		for(int i = 0; i < n; i++) {
+			input[i] = i+1;
+		}
+		permu(0, 0);
+		System.out.println(sb.toString());
+		
+	}
+	public static void permu(int start, int cnt) {
+		if(cnt == m) {
+			for(int i = 0; i < m; i++) {
+				sb.append(numbers[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = start; i < n; i++) {
+			if(isSelected[i]) continue;
+			numbers[cnt] = input[i];
+			isSelected[i] = true;
+			permu(i+1, cnt+1);
+			isSelected[i] = false;
+		}
+	}
+}

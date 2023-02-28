1589841cf65f0a41e441a8250983c7972ff8d9dd
lapaho8645 <64197428+lapaho8645@users.noreply.github.com>
2023-02-28 18:21:38 +0900

    [Gold III] Title: ACM Craft, Time: 948 ms, Memory: 301036 KB -BaekjoonHub

---
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++){
+            String [] input = br.readLine().split(" ");
+            int N = Integer.parseInt(input[0]);
+            int K = Integer.parseInt(input[1]);
+            int time [] = new int[N+1];
+            input = br.readLine().split(" ");
+            int [] sum = new int[N+1];
+            for (int i = 1; i <= N; i++){
+                time[i] = Integer.parseInt(input[i-1]);
+                sum[i] = time[i];
+            }
+            int [] inCount = new int[N+1];
+            Node [] list = new Node[N+1];
+            for (int i = 0; i < K; i++){
+                input = br.readLine().split(" ");
+                int first = Integer.parseInt(input[0]);
+                int second = Integer.parseInt(input[1]);
+                list[first] = new Node(second, list[first]);
+                inCount[second]++;
+            }
+            Queue<int []> queue = new LinkedList<>();
+            for (int i = 1; i <= N; i++){
+                if (inCount[i] == 0){
+                    queue.add(new int[] {i, 0, i}); //vertex, depth
+                }
+            }
+            int W = Integer.parseInt(br.readLine());
+
+            while (!queue.isEmpty()){
+                int [] cur = queue.poll();
+
+                Node curNode = list[cur[0]];
+                while (curNode != null){
+                    sum[curNode.vertex] = Math.max(sum[curNode.vertex], sum[cur[0]] + time[curNode.vertex]);
+                    if(--inCount[curNode.vertex] == 0){
+                        queue.add(new int [] {curNode.vertex, cur[1] + 1, cur[2]});
+                    }
+                    curNode = curNode.link;
+                }
+            }
+            System.out.println(sum[W]);
+        }
+
+    }
+    public static class Node{
+        int vertex;
+        Node link;
+        Node(int vertex, Node link){
+            this.vertex = vertex;
+            this.link = link;
+        }
+    }
+}
